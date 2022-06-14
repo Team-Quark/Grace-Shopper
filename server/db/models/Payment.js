4c7a7125805ea161fcbdc67926a0e3a4eda0b0d1
Nikul Patel <nikulp1997@gmail.com>
2022-06-14 17:35:06 -0400

    Payment Model Updated

---
+++ server/db/models/Payment.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+
+const Payment = db.define('payment', {
+  ccNo: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isCreditCard: true, // Double check this
+    },
+  },
+  expDate: {
+    type: Sequelize.DATEONLY,
+    allowNull: false,
+  },
+  securityCode: {
+    type: Sequelize.STRING,
+    validate: {
+      len: [3, 4], //allows length between 3 and 4
+    },
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Payment;
